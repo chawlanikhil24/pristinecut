eca2d693a47475481990b2aaec12d71e71f75879
Saurabh Badhwar <sbsaurabhbadhwar9@gmail.com>
2016-03-19 11:01:49 +0530

    Cleanup

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,43 @@
-from flask import Flask,render_template,Response,session,request,redirect,url_for,g
+from flask import Flask,render_template,Response,session,request,redirect,url_for
 import web,requests
 from facepy import GraphAPI
 from urlparse import parse_qs
 
-app=Flask(__name__)
-app.secret_key="Naachore"
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+app = Flask(__name__)
+app.secret_key = 'Fuckedupworld'
 
 fb_app_id="209704059386926"
 fb_app_secret_key="93895f35938943ab0fd709530b9e3a97"
-post_login_url="127.0.0.1:5000"
-#Setup routing
-@app.route("/",methods=['GET','POST'])
+post_login_url="http://localhost:5000/"
+#Just Some Ping Tests
+@app.route('/ping')
+def ping():
+
+	return "Hello World"
+
+#-----------------------------Here Goes All The Routing API's--------------------------------------------------#
+@app.route('/',methods=['GET','POST'])
 def main():
-    user_data=web.input(code=None)
-    if not user_data.code:
-        dialog_url=("http://www.facebook.com/dialog/oauth?"+"client_id="+fb_app_id+"&redirect_uri="+post_login_url+"&scope=publish_stream")
+    if "code" not in request.values.keys():
+        dialog_url=("http://www.facebook.com/dialog/oauth?"+"client_id="+fb_app_id+"&redirect_uri="+post_login_url+"&scope=publish_actions")
         return redirect(dialog_url)
     else:
+        code=request.values.get("code")
         graph=GraphAPI()
+        response=graph.get(
+            path='oauth/access_token',
+            client_id=fb_app_id,
+            client_secret=fb_app_secret_key,
+            redirect_uri=post_login_url,
+            code=code
+        )
+        data=parse_qs(response)
+        graph=GraphAPI(data['access_token'][0])
+        graph.post(path='me/feed',message='test post')
+        return data['access_token'][0]
 
 
-#code for photo upload
 
-photos = UploadSet('photos',IMAGES)
-
-@app.route('/upload',methods=['GET','POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        rec = Photo(filename=filename. user=g.user.id)
-        rec.store()
-        flash("Photo Saved")
-        return redirect(url_for('show', id=rec.id))
-    return render_template('upload.html')
-
-@app.route('/photo/<id>')
-def show(id):
-    photo = Photo.load(id)
-    if photo is None:
-        abort(404)
-    url = photos.url(photo.filename)
-    return render_template('show.html', url=url, photo=photo)
+if __name__ == "__main__":
+	app.run(debug=True)
